5b72bead8a1d65706cd355b6b47465f9cdaf73ce
zahirraihan26 <mdzahirraihanbakul@gmail.com>
2025-10-17 21:13:12 +0600

    added footer section

--- src/Head/Head.jsx
+++ src/Head/Head.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 const Head = () => {
     return (
         <div className='flex flex-col min-h-screen'>
             <Navbar></Navbar>
+
+            
+
+            <Footer></Footer>
         </div>
     );
 };
